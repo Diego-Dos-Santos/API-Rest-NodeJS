e2802fcb596cc7e1b0559e8312eb1091dc851bc9
Diego dos Santos <diegodossantos.es@gmail.com>
2019-06-07 14:29:32 +0200

    GET from data base

--- index.js
+++ index.js
@@ -16,11 +16,23 @@ app.use(express.json());
 
 //petición GET de la tienda online
 app.get('/api/product', (req, res) => {
-    res.status(200).send({products:[]})
+    Product.find({}, (err, products) => {
+    if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
+    if (!products) return res.status(404).send({message: `Ese producto no existe`})
+
+    res.status(200).send({products})
+    })
 })
 
-app.get('/api/:productId', (req,res) => {
+app.get('/api/product/:productId', (req,res) => {
+    let productId = req.params.productId
 
+    Product.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
+        if (!product) return res.status(404).send({message:`El producto no existe`})
+        
+        res.status(200).send({ product })
+    })
 })
 
 //petición POST de la tienda online 
